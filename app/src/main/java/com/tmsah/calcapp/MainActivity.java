3add1851fea69a426bec95744af9a1ba16a798b6
tmsah <hideki81000@gmail.com>
2018-12-30 23:22:49 +0900

    ベースとなる動作の実装

--- app/src/main/java/com/tmsah/calcapp/MainActivity.java
+++ app/src/main/java/com/tmsah/calcapp/MainActivity.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    TextView sum;
+    TextView num;
+    TextView average;
     EditText editText;
     Button button;
 
@@ -19,9 +22,15 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             recentOperator = R.id.button_equal;
             result = 0;
+            numNum = 0;
+            sumNum = 0;
+            isTensPlace = true;
             isOperatorKeyPushed = false;
 
-            textView.setText("");
+            textView.setText("input:---");
+            sum.setText("sum:---");
+            num.setText("num:---");
+            average.setText("ave:---");
             editText.setText("");
         }
     };
@@ -31,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.textview);
+        sum = (TextView) findViewById(R.id.sum);
+        num = (TextView) findViewById(R.id.num);
+        average = (TextView) findViewById(R.id.average);
         editText= (EditText) findViewById(R.id.edittext);
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(buttonListener);
@@ -45,22 +57,27 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_8).setOnClickListener(buttonNumberListener);
         findViewById(R.id.button_9).setOnClickListener(buttonNumberListener);
         findViewById(R.id.button_0).setOnClickListener(buttonNumberListener);
-        findViewById(R.id.button_dot).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_00).setOnClickListener(buttonNumberListener);
         findViewById(R.id.button_add).setOnClickListener(buttonOperatorListener);
         findViewById(R.id.button_subtract).setOnClickListener(buttonOperatorListener);
         findViewById(R.id.button_multiply).setOnClickListener(buttonOperatorListener);
         findViewById(R.id.button_divide).setOnClickListener(buttonOperatorListener);
         findViewById(R.id.button_equal).setOnClickListener(buttonOperatorListener);
     }
+
     int recentOperator = R.id.button_equal; // 最近押された計算キー
-    double result;  // 計算結果
+    float result;  // 計算結果
     boolean isOperatorKeyPushed;    // 計算キーが押されたことを記憶
+    int numNum;
+    int sumNum;
+    boolean isTensPlace = true;
+    String br = System.getProperty("line.separator");  //改行コード
 
     View.OnClickListener buttonOperatorListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             Button operatorButton = (Button) view;
-            double value = Double.parseDouble(editText.getText().toString());
+            float value = Float.parseFloat(editText.getText().toString());
             if (recentOperator == R.id.button_equal) {
                 result = value;
             } else {
@@ -77,17 +94,33 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             Button button = (Button) view;
+            editText.append(button.getText());
 
-            if (isOperatorKeyPushed == true) {
-                editText.setText(button.getText());
-            } else {
-                editText.append(button.getText());
+            if (isTensPlace) {
+                isTensPlace = false;
+            }
+            else{
+                int value = Integer.parseInt(editText.getText().toString());
+                if (numNum == 0) {
+                    textView.setText("input:" + br + String.valueOf(value));
+                }
+                else {
+                    textView.append(br + String.valueOf(value));
+                }
+                editText.setText("");
+                numNum++;
+                sumNum+=value;
+                result = (float) sumNum / numNum;
+                sum.setText("sum:" + String.valueOf(sumNum));
+                num.setText("num:" + String.valueOf(numNum));
+                average.setText("ave:" + String.format("%.1f", result));
+                isTensPlace = true;
             }
 
             isOperatorKeyPushed = false;
         }
     };
-    double calc(int operator, double value1, double value2) {
+    float calc(int operator, float value1, float value2) {
         switch (operator) {
             case R.id.button_add:
                 return value1 + value2;
